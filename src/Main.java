ecae49c436af3b0df45c3b3ae24875024b52ab81
Gabriel.Mach <gabriel.luizzmac@gmail.com>
2022-12-03 02:09:08 -0300

    Prontinho Krlh

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,40 @@
+import cliente.CadastroCliente;
 import cliente.Cliente;
 import db.*;
+import exceptions.CJCException;
 import exceptions.CPFIExecption;
 import exceptions.FJCFException;
 import funcionario.CadastroFuncionario;
 import funcionario.Empregado;
 import pedido.CadastroPedido;
+import pedido.Pedido;
+import pratos.CadastroPrato;
+import pratos.Prato;
 
 import java.util.Scanner;
 
 import static java.lang.System.*;
 
 public class Main {
-    public static void main(String[] args) throws FJCFException, CPFIExecption {
+    public static void main(String[] args) throws FJCFException, CPFIExecption, CJCException {
         RepositorioPedidos rp = new RepositorioPedidosLista();
         //RepositorioPedidos rp = new RepositorioPedidosVetor();
-        CadastroPedido cadastroPedido = new CadastroPedido(rp);
+        CadastroPedido cad = new CadastroPedido(rp);
+
         RepositorioFuncionario rf = new RepositorioFuuncionarioLista();
         CadastroFuncionario cadF = new CadastroFuncionario(rf);
+
+        RepositorioPratoLista pr = new RepositorioPratoLista();
+        CadastroPrato cadP = new CadastroPrato(pr);
+
+        RepositorioClienteLista cliL = new RepositorioClienteLista();
+        CadastroCliente cadCl = new CadastroCliente(cliL);
 
 
 
         Scanner s = new Scanner(in);
 
-        Cliente cli = new Cliente();
+
 
         out.println("Bem Vindo a Cafeteria Baby Reborn");
         out.println("Você é:\n1- Cliente\n2- Funcionario");
@@ -30,16 +42,44 @@ public class Main {
 
         if (res == 1){
             int r1;
-            out.println("1- Cardápio\n2- Seu Pedido\n3- Finalizar pedido\n 4- Sair");
+            out.println("1- Cardápio\n2- Seu Pedido\n 3- Sair");
             //r1 = Integer.parseInt(s.next());
             r1 = s.nextInt();
             while (r1!=4) {
                 if (r1 == 1){
-
+                    out.println(pr);
 
                 } else if (r1 == 2) {
+                    String nmCl, clCPF, clEnd, numCl;
+                    out.println("Nome do Cliente:");
+                    nmCl = s.nextLine();
 
-                } else if (r1 == 3) {
+                    out.println("CPF:");
+                    clCPF = s.nextLine();
+
+                    out.println("Endereço:");
+                    clEnd = s.nextLine();
+
+                    out.println("Numero:");
+                    numCl = s.nextLine();
+
+                    Cliente cli = new Cliente(nmCl,clCPF,clEnd,numCl);
+
+                    cadCl.cadastrar(cli);
+
+                    out.println("Pedido do Cliente");
+                    String com = s.nextLine();
+
+                   while (com != "SAIR"){
+                        out.println("Mais um pedido?\n Se sim escolha\n se não, escreva SAIR");
+                       com = s.nextLine();
+
+                   }
+
+                   Pedido p1 = new Pedido(cli);
+
+                   cad.cadastrar(p1);
+
 
                 } else {
                     out.println("Opção Inválida");
@@ -70,6 +110,16 @@ public class Main {
                     cadF.cadastrar(e1);
 
                 } else if (r2 == 2) {
+                    out.println("Nome do Prato:");
+                    String nmp = s.nextLine();
+
+                    out.println("Preço do Prato:");
+                    double prp = s.nextDouble();
+
+                    Prato p1 = new Prato(nmp,prp);
+                    cadP.cadastrar(p1);
+
+
 
 
                 } else {
